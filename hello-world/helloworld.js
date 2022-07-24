56233744faa94a8d2fffbc27366f1b4997192b4a
Arnaud Lauret <arno.di.loreto@gmail.com>
2022-07-24 16:04:37 +0200

    vanilla js web component: using closed shadow DOM and inner CSS

--- hello-world/helloworld.js
+++ hello-world/helloworld.js
@@ -6,6 +6,9 @@ class HelloWorld extends HTMLElement {
   constructor() {
     super();
     this.name = "World"; // Default value
+    // Using "closed" shadow DOM, it can 
+    // only be accessed within the web component
+    this.shadow = this.attachShadow({ mode: 'closed' });
   }
   
   // The attributes of the web component
@@ -16,9 +19,24 @@ class HelloWorld extends HTMLElement {
 
   // Custom method centralizing UI modifications
   _render() {
-    this.innerHTML = `
-      <div><h5>Greetings message</h5><p>Hello ${this.name}</p></div>
-    `;
+      // Different style from helloworld.css, this one will be applied
+      this.shadow.innerHTML = `
+        <style>
+        p {
+          font-weight: italic;
+          font-size: 1.5rem;
+          color: red;
+          text-align: center;
+          font-weight: normal;
+          padding: 0.5rem;
+          margin: 0 0 1rem 0;
+          background-color: #eee;
+          border: 1px solid blue;
+        }
+        </style>
+
+        <div><h5>Greetings message (with closed shadow DOM)</h5><p>Hello ${this.name}</p></div>
+      `;
   }
 
   // Callback called when an attribute is set in tag or changed
